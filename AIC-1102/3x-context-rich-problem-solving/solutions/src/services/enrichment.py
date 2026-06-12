e55c769727009dea67a1113f75549c295e3b9033
Alberto Gutierrez <alberto.gutierrez2@fedex.com>
2026-06-12 14:19:33 -0700

    lab notes

--- AIC-1102/3x-context-rich-problem-solving/solutions/src/services/enrichment.py
+++
@@ -1,5 +0,0 @@
-def enrich(event):
-    if event.metadata is None:
-        event.metadata = {}
-    event.metadata["customer_prefix"] = event.customer_id[:3] if event.customer_id else ""
-    return event
